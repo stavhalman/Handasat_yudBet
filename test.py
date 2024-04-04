11c7aaef74fdc63518ad97f0063615363f1f91d6
stavhalman <stav.halman@gmail.com>
2024-04-04 12:54:14 +0300

    0.12

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-picturePath = 'itaygaming.png'
-with open (picturePath,'rb') as file:
-    data=file.read()
-print(len(data)/1024)
+
+import cv2
+  
+
+cam_port = 0
+cam = cv2.VideoCapture(cam_port) 
+  
+result, image = cam.read() 
+  
+
+if result: 
+
+    cv2.imshow("GeeksForGeeks", image) 
+  
+
+    cv2.imwrite("Handasat_yudBetPicture.png", image) 
+  
+    cv2.waitKey(0) 
+    cv2.destroyWindow("GeeksForGeeks") 
+
