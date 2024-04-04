0dfab9eda23917f57d8ace01fe6f07eb81a1fe1d
stavhalman <stav.halman@gmail.com>
2024-04-04 13:09:18 +0300

    1.12

--- client.py
+++ client.py
@@ -7,6 +7,5 @@ serverAddress = ('10.0.0.2',8888)
 UDPClient=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 UDPClient.connect(serverAddress)
 
-protocol.sendMessage("takePicture()","do",UDPClient)
 protocol.reciveMessage(UDPClient)
 protocol.reciveMessage(UDPClient)
