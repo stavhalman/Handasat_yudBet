192454c0b6f3e929bfad23adf6e79b56224a7887
stavhalman <stav.halman@gmail.com>
2024-05-02 13:35:43 +0300

    1.13

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import protocol
 
 
-serverAddress = ('10.0.0.2',8888)
+serverAddress = ('10.0.0.22',8888)
 
 UDPClient=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 UDPClient.connect(serverAddress)
